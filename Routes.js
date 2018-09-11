b12a44aec2ccaf72decd30d966fbc56c7fde9839
Tiago Almeida <thyagoeap@gmail.com>
2018-09-11 12:03:46 -0300

    comentarios

--- Routes.js
+++ Routes.js
@@ -1,13 +1,22 @@
-//import React, {Components} from 'react';
+/*
+    esse arquivo indexa todas as telas do aplicativo,
+    toda tela precisa esta indexada aqui pra ser chamada.
+*/ 
 import {createStackNavigator} from 'react-navigation';
 
+//tela de atrações turisticas
 import AttractionsScreen from './src/components/attractions/AttractionsScreen';
 
+/* 
+    RootStack recebe um React.Component do createStackNavigator
+    Assim podemos exporta é utilizar como um component no App.js
+*/
 const RootStack = createStackNavigator(
-    {
+    {   //lista de telas
         Attractions: AttractionsScreen
     },
     {
+        //configurações
         initialRouteName: 'Attractions',
         headerMode: 'none'
     }
