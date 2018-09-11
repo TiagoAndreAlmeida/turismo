1c07f818b7bd89728b04b6fb49a3a4393d2b0f26
Tiago Almeida <thyagoeap@gmail.com>
2018-09-11 12:04:46 -0300

    touchable nas postagem

--- src/components/attractions/AttractionsScreen.js
+++ src/components/attractions/AttractionsScreen.js
@@ -1,24 +1,20 @@
 import React, {Component} from 'react';
-import {Image, FlatList} from 'react-native';
+import {Image, FlatList, TouchableWithoutFeedback} from 'react-native';
 import {Container, Header, Title, Right, Card, CardItem, Left, Body, Content, Text} from 'native-base';
 
 const data = require('../../../pontos.json');
 export default class AttractionsScreen extends Component {
     constructor(props){
         super(props);
-        this.state = {
-            uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/CHAPADA_DO_ARARIPE_01.jpg/300px-CHAPADA_DO_ARARIPE_01.jpg",
-            title: "Chapada do Araripe",
-            descib: "Localizada ao Sul no Estado, na região do Cariri, a Chapada do Araripe se encontra na divisa entre o Ceará e Pernambuco e é uma reserva ecológica que reúne fontes naturais, grutas e sítios paleontológicos"
-        }
     }
+
     render() {
       return (
         <Container>
           <Header>
             <Left/>
             <Body>
-              <Title>Header</Title>
+              <Title>Atrações</Title>
             </Body>
             <Right />
           </Header>
@@ -26,6 +22,7 @@ export default class AttractionsScreen extends Component {
           <Content>
             <FlatList
             data={data}
+            keyExtractor={item => item.title}
             renderItem={({item}) => 
             <Card style={{flex: 0}}>
               <CardItem>
@@ -37,10 +34,10 @@ export default class AttractionsScreen extends Component {
               </CardItem>
               <CardItem>
                 <Body>
-                  <Image source={{uri: item.uri }} style={{height: 200, width: 350, flex: 1}}/>
-                  <Text>
-                    {item.descib}
-                  </Text>
+                  <TouchableWithoutFeedback
+                  onPress={() => alert(item.title)}>
+                    <Image source={{uri: item.uri }} style={{height: 200, width: 325, flex: 1}}/>
+                  </TouchableWithoutFeedback>
                 </Body>
               </CardItem>
             </Card> }
